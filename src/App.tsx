d27788bf1081253d1023c9f2a14d2fcfde3b2d30
Ken <ken.aleksander@gmail.com>
2022-06-28 11:53:49 +0200

    justert eksempel

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,26 @@ import { useState } from "react";
 import LoginForm from "./LoginForm";
 
 function App() {
+  /* Håndterer åpen-state for Modal */
   const [openLogin, setOpenLogin] = useState(false);
 
   return (
     <div className="bg-gray-50 w-screen h-screen grid place-content-center">
+      {/* Knapp med login-ikon */}
       <Button onClick={() => setOpenLogin(true)} variant="secondary">
         <Login aria-hidden /> Logg inn
       </Button>
+
+      {/* Modal uten lukkeknapp og mulighet til å lukkes  */}
       <Modal
         className="w-full max-w-xl"
         open={openLogin}
-        onClose={() => null}
+        onClose={() => setOpenLogin(false)}
         shouldCloseOnOverlayClick={false}
         closeButton={false}
       >
         <Modal.Content className="p-6 w-full">
+          {/* Importert skjema-komposisjon */}
           <LoginForm onClose={() => setOpenLogin(false)} />
         </Modal.Content>
       </Modal>
--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,8 +1,14 @@
 import { BodyShort, Button, Heading, TextField } from "@navikt/ds-react";
 
 const LoginForm = ({ onClose }: { onClose: () => void }) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    /* Håndter Login her! */
+  };
+
   return (
     <div className="grid gap-8">
+      {/* Tittel og beskrivelse */}
       <div>
         <Heading size="xlarge" level="1" spacing>
           Logg inn
@@ -12,16 +18,22 @@ const LoginForm = ({ onClose }: { onClose: () => void }) => {
           At last we have the mighty Chewbacca.
         </BodyShort>
       </div>
-      <div className="grid gap-6">
-        <TextField label="E-post" htmlSize={35} />
-        <TextField label="Passord" type="password" htmlSize={35} />
-      </div>
-      <div className="flex justify-items-start gap-6">
-        <Button onClick={() => onClose()}>Logg inn</Button>
-        <Button onClick={() => onClose()} variant="secondary">
-          Avbryt
-        </Button>
-      </div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        {/* Input-felter */}
+        <div className="grid gap-6">
+          <TextField label="E-post" htmlSize={35} />
+          <TextField label="Passord" type="password" htmlSize={16} />
+        </div>
+        {/* Innsendings-knapper */}
+        <div className="flex justify-items-start gap-6 mt-8">
+          <Button type="submit" onClick={() => onClose()}>
+            Logg inn
+          </Button>
+          <Button onClick={() => onClose()} variant="secondary">
+            Avbryt
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
